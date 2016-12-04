fe1ab76bdde70c2fb8c972bf9bdca09970d9caf4
Justin Ith <jith@u.washington.edu>
2016-12-04 14:13:17 -0800

    Added dynamic date rendering and basic ability to add phases

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 (function() {
 
     window.onload = function(){
-    	
+
+        renderDateRow();
+
+    	$('.dayBlock').click(function(){
+            var newBlock = addBlock("Testing");
+            $(this).append(newBlock);
+        });
+
+        var day = moment(new Date().getTime()).format('M' + '/' + 'D' + '<br>' + 'dd');
+
+        console.log(day);
+    }
+
+    function renderDateRow(){
+
+        var dayMilli = new Date().getTime();
+
+        var currentDayFormatted = moment(dayMilli).format('M' + '/' + 'D' + '<br>' + 'dd');
+
+        var start = '<div class="row"><div class="col-sm-offset-1 col-sm-1">' + currentDayFormatted + '</div>';
+
+        for(var i = 0; i <= 8; i++){
+            dayMilli += 86400000;
+            dayFormatted = moment(dayMilli).format('M' + '/' + 'D' + '<br>' + 'dd');
+            start += '<div class="col-sm-1">' + dayFormatted + '</div>'
+        }
+
+        start += '</div>';
+
+        $('#dateDayRow').append(start);
+    }
+
+    function addBlock(text){
+        var projectText = '<div class="projectHolder"><div class="dayContent"><div class="dayTitle">' + text + '</div></div></div>';
+
+        return projectText;
     }
 
 })();
