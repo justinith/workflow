5caa289b156aff01d7a48b1fbd0a8e84ef2492dd
yangf6 <yangf6@uw.edu>
2016-12-04 15:08:37 -0800

    popover bug need fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
-(function() {
+"use strict";
+$(document).ready(function(){
+    $("[data-toggle=popover]").popover({
+        html: true, 
+        content: function() {
+            return $('#popover-content').html();
+        }
+    })
+    $(document.body).on('submit','#class-add-form',function (e) {
+        e.preventDefault();
+        var classToAdd = document.getElementById("class-name");
+        alert('Form submitted');
+        console.log("length", classToAdd.value.length);
+    })
+})
 
-    window.onload = function(){
-    	
-    }
-
-})();
+function myFunction() {
+    var classAdd = document.getElementById("class-name");
+       console.log(classAdd.value);
+}
+// var addClass = document.getElementById("class-add-form");
+//         addClass.addEventListener("submit", function(){
+//         var classToAdd = addClass.querySelector(".class-name");
+//         console.log(classToAdd.value);
+//         });
+// });
