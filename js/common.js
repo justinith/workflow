17a5391f4165a5fc2137211275659a1357e407d7
Marika Rundle <mrundle7@uw.edu>
2016-12-04 15:11:28 -0800

    signup and signin

---
+++ js/common.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var config = {
+    apiKey: "AIzaSyDkBifE9dCgqzn4ivf5uD7RXSwfN99Na_o",
+    authDomain: "workflow-462a4.firebaseapp.com",
+    databaseURL: "https://workflow-462a4.firebaseio.com",
+    storageBucket: "workflow-462a4.appspot.com",
+    messagingSenderId: "889877406021"
+};
+firebase.initializeApp(config);
